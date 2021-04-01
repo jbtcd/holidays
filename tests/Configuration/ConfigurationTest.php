f3ee02dc9c6986f29d89236d99bebe218ad3e9af
Jonah Böther <mail@jbtcd.me>
2021-04-01 23:31:59 +0200

    Fix and extends configuration tests

--- tests/Configuration/ConfigurationTest.php
+++ tests/Configuration/ConfigurationTest.php
@@ -3,17 +3,45 @@
 namespace jbtcd\HolidaysTest\Configuration;
 
 use jbtcd\Holidays\Configuration\Configuration;
+use jbtcd\Holidays\Countries\Germany;
+use jbtcd\Holidays\Exception\CountryNotSupportedException;
 use PHPUnit\Framework\TestCase;
 
 class ConfigurationTest extends TestCase
 {
     /**
-     * @covers ::isCountrySupported
+     * @covers \jbtcd\Holidays\Configuration\Configuration::isCountrySupported
      */
     public function testIfCountryIsAvailable(): void
     {
-        $this->assertTrue(
-            Configuration::isCountrySupported('DEU')
-        );
+        $this->assertTrue(Configuration::isCountrySupported('DEU'));
+    }
+
+    /**
+     * @covers \jbtcd\Holidays\Configuration\Configuration::isCountrySupported
+     */
+    public function testIfCountryIsNotAvailable(): void
+    {
+        $this->assertFalse(Configuration::isCountrySupported('USA'));
+    }
+
+    /**
+     * @covers \jbtcd\Holidays\Configuration\Configuration::getCountryClass
+     * @covers \jbtcd\Holidays\Configuration\Configuration::isCountrySupported
+     */
+    public function testIfCountryReturnsCorrectClass(): void
+    {
+        $this->assertEquals((new Germany()), Configuration::getCountryClass('DEU'));
+    }
+
+    /**
+     * @covers \jbtcd\Holidays\Configuration\Configuration::getCountryClass
+     * @covers \jbtcd\Holidays\Configuration\Configuration::isCountrySupported
+     */
+    public function testForCountryNotSupportedException(): void
+    {
+        $this->expectException(CountryNotSupportedException::class);
+
+        Configuration::getCountryClass('USA');
     }
 }
