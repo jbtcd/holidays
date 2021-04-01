4d22dc0e6a6f56610403c2bf4dde98402f1c9c62
Jonah Böther <mail@jbtcd.me>
2021-04-01 23:09:19 +0200

    Add first test for configuration

---
+++ tests/Configuration/ConfigurationTest.php
@@ -0,0 +1,16 @@
+<?php declare(strict_types = 1);
+
+namespace jbtcd\HolidaysTest\Configuration;
+
+use jbtcd\Holidays\Configuration\Configuration;
+use PHPUnit\Framework\TestCase;
+
+class ConfigurationTest extends TestCase
+{
+    public function testIfCountryIsAvailable(): void
+    {
+        $this->assertTrue(
+            Configuration::isCountrySupported('DEU')
+        );
+    }
+}
